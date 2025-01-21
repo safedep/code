a3616a72643144fe2e9a605b11dc95c7613a3457
Omkar Phansopkar <omkarphansopkar@gmail.com>
2025-01-21 10:40:57 +0530

    API for plugin integrations in malysis (#14)
    * Experiment API
    
    Signed-off-by: Omkar Phansopkar <omkarphansopkar@gmail.com>
    
    * Added documentation for io reader and ResolveLanguage APIs
    
    Signed-off-by: Omkar Phansopkar <omkarphansopkar@gmail.com>
    
    ---------
    
    Signed-off-by: Omkar Phansopkar <omkarphansopkar@gmail.com>

--- lang/factory.go
+++ lang/factory.go
@@ -2,6 +2,8 @@ package lang
 
 import (
 	"fmt"
+	"path/filepath"
+	"slices"
 
 	"github.com/safedep/code/core"
 )
@@ -21,4 +23,25 @@ func GetLanguage(name string) (core.Language, error) {
 	}
 
 	return nil, fmt.Errorf("language not found: %s", name)
+}
+
+// ResolveLanguageFromPath resolves the programming language from the
+// filePath and returns the core.Language and a boolean indicating if the
+// language implementation exists for the specified file extension in filePath.
+//
+// It returns nil, false if the file extension is not supported by any implemented language.
+func ResolveLanguageFromPath(filePath string) (core.Language, bool) {
+	extension := filepath.Ext(filePath)
+
+	for _, f := range languages {
+		l, err := f()
+		if err != nil {
+			return nil, false
+		}
+
+		if slices.Contains(l.Meta().SourceFileExtensions, extension) {
+			return l, true
+		}
+	}
+	return nil, false
 }
