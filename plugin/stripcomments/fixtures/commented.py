8d666239436593a334f6740886f105c2c527e35c
Omkar Phansopkar <omkarphansopkar@gmail.com>
2025-01-27 13:33:18 +0530

    Migrated stripcomments from recursive to iterative
    Signed-off-by: Omkar Phansopkar <omkarphansopkar@gmail.com>

--- plugin/stripcomments/fixtures/commented.py
+++ plugin/stripcomments/fixtures/commented.py
@@ -15,6 +15,33 @@ def func(arg: str):
         """
         Docstring inside nested func
         """
+        if 4<9:
+            if 5<3:
+                print("deeper_nested_func")
+                # single line comment
+            else:
+                print("deeper_nested_func")
+                while(1):
+                    print("deepest_nested_func") ###### comment here
+                    if 5>3:
+                        break
+                match 'a':
+                    case 'a':
+                        if 3<10:
+                            print("a_if")
+                            '''further comment'''
+                        print("a")
+                    case 'b':
+                        match "zs":
+                            # single line comment
+                            case "zs":
+                                print("zs")
+                            case _:
+                                print("default")
+                        print("b") # some comments
+                    case _:
+                        print("default")###
+            print("deep_nested_func")
         print("nested_func")#
         pass
     nested_func()
--- plugin/stripcomments/fixtures/stripped.py
+++ plugin/stripcomments/fixtures/stripped.py
@@ -9,6 +9,33 @@ def func(arg: str):
     print("func-", arg) 
     def nested_func():
         
+        if 4<9:
+            if 5<3:
+                print("deeper_nested_func")
+                
+            else:
+                print("deeper_nested_func")
+                while(1):
+                    print("deepest_nested_func") 
+                    if 5>3:
+                        break
+                match 'a':
+                    case 'a':
+                        if 3<10:
+                            print("a_if")
+                            
+                        print("a")
+                    case 'b':
+                        match "zs":
+                            
+                            case "zs":
+                                print("zs")
+                            case _:
+                                print("default")
+                        print("b") 
+                    case _:
+                        print("default")
+            print("deep_nested_func")
         print("nested_func")
         pass
     nested_func()
